fc9a6126a702029036fc72e8e534189bd677f4dd
BigBrainAhmed <94079747+BigBrainAhmed@users.noreply.github.com>
2023-02-04 15:38:03 -0500

    Working Gyro Program

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,6 +11,21 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.ADIS16470_IMU;
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 
 /**
  * This is a sample program to demonstrate how to use a gyro sensor to make a robot drive straight.
@@ -20,39 +35,45 @@ import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
  
 public class Robot extends TimedRobot {
-  private static final double kAngleSetpoint = 0.0;
-  private static final double kP = 0.005; // propotional turning constant
 
-  // gyro calibration constant, may need to be adjusted;
-  // gyro value of 360 is set to correspond to one full revolution
-  // private static final double kVoltsPerDegreePerSecond = 0.0128;
+  private ADIS16470_IMU imu = new ADIS16470_IMU();
+  private final Joystick m_joystick = new Joystick(0);
 
+  private GenericEntry m_maxSpeed;
+  private NetworkTableEntry shuffle_angle;
   
-  private static final int kGyroPort = 0;
-  private static final int kJoystickPort = 0;
-
-  
-  
-  private final AnalogGyro m_gyro = new AnalogGyro(kGyroPort);
-  private final Joystick m_joystick = new Joystick(kJoystickPort);
 
   @Override
-  public void robotInit() {
-    // m_gyro.setSensitivity(kVoltsPerDegreePerSecond);
-    m_gyro.calibrate();
+  public void robotInit(){
+    imu.calibrate();
+
+
+    // Add the tank drive and encoders to a 'Drivebase' tab
+
+    /*// Put both encoders in a list layout
+    ShuffleboardLayout encoders =
+        driveBaseTab.getLayout("List Layout", "Encoders").withPosition(0, 0).withSize(2, 2);
+    encoders.add("Left Encoder", m_leftEncoder);
+    encoders.add("Right Encoder", m_rightEncoder);
+
+    // Add the elevator motor and potentiometer to an 'Elevator' tab
+    ShuffleboardTab elevatorTab = Shuffleboard.getTab("Elevator");
+    elevatorTab.add("Motor", m_elevatorMotor);
+    elevatorTab.add("Potentiometer", m_elevatorPot);
+
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
+    */
   }
 
   /**
    * The motor speed is set from the joystick while the DifferentialDrive turning value is assigned
    * from the error between the setpoint and the gyro angle.
    */
-
   @Override
   public void teleopPeriodic() {
-    // System.out.println(m_gyro.getAngle());
-    System.out.println(Rotation2d.fromDegrees(-m_gyro.getAngle()));
+    System.out.println(imu.getAngle());
+    SmartDashboard.putNumber("Bridge Angle", imu.getAngle());
   }
 }
