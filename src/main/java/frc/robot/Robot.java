890e12446c92cfdbb5234f29a29265214e460383
BigBrainAhmed <94079747+BigBrainAhmed@users.noreply.github.com>
2023-12-02 14:38:21 -0500

    Swerve

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,240 +1,180 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
-
-import edu.wpi.first.wpilibj.Joystick;
+import com.pathplanner.lib.server.PathPlannerServer;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import java.io.File;
+import java.io.IOException;
+import swervelib.parser.SwerveParser;
 
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to each mode, as
+ * described in the TimedRobot documentation. If you change the name of this class or the package after creating this
+ * project, you must also update the build.gradle file in the project.
+ */
+public class Robot extends TimedRobot
+{
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+  private static Robot   instance;
+  private        Command m_autonomousCommand;
 
+  private RobotContainer m_robotContainer;
 
-import edu.wpi.first.wpilibj.motorcontrol.*;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+  private Timer disabledTimer;
 
-import java.nio.file.attribute.AclEntryPermission;
+  public Robot()
+  {
+    instance = this;
+  }
 
+  public static Robot getInstance()
+  {
+    return instance;
+  }
 
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.cscore.UsbCamera;
-import edu.wpi.first.cscore.VideoSink;
-import edu.wpi.first.cscore.VideoSource;
-import edu.wpi.first.cscore.VideoSource.ConnectionStrategy;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
+  /**
+   * This function is run when the robot is first started up and should be used for any initialization code.
+   */
+  @Override
+  public void robotInit()
+  {
+    PathPlannerServer.startServer(5811);
+    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
+    m_robotContainer = new RobotContainer();
 
+    // Create a timer to disable motor brake a few seconds after disable.  This will let the robot stop
+    // immediately when disabled, but then also let it be pushed more 
+    disabledTimer = new Timer();
+  }
 
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics that you want ran
+   * during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic()
+  {
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
+  }
 
-public class Robot extends TimedRobot {
+  /**
+   * This function is called once each time the robot enters Disabled mode.
+   */
+  @Override
+  public void disabledInit()
+  {
+    m_robotContainer.setMotorBrake(true);
+    disabledTimer.reset();
+    disabledTimer.start();
+  }
 
-private static final int rightMotor0ID = 1;
-private static final int rightMotor1ID = 2;
-private static final int leftMotor0ID = 3;
-private static final int leftMotor1ID = 4;
-private static final int armRetracterID = 6;
-private static final int armRaiserID = 7;
-
-private CANSparkMax leftMotor0;
-private CANSparkMax leftMotor1;
-private CANSparkMax rightMotor0;
-private CANSparkMax rightMotor1;
-private CANSparkMax armRetracter;
-private CANSparkMax armRaiser;
-
-private DoubleSolenoid solenoid = new DoubleSolenoid(10, PneumaticsModuleType.REVPH, 7, 5);
-
-
-double percentPower = 0.8;
-
-
-private Timer elapsedTime = new Timer();
-
-
-private final Joystick m_stick = new Joystick(0);
-private DifferentialDrive m_robotDrive;
-
-UsbCamera camera1;
-UsbCamera camera2;
-
-NetworkTableEntry cameraSelection;
-
-VideoSink server;
-
-boolean cameraSwitch = false;
-boolean inEncoderLimit = true;
-
-boolean disableEncoder;
-
-
-
-@Override
-public void robotInit(){
-
-  camera1 = CameraServer.startAutomaticCapture(0);
-  camera2 = CameraServer.startAutomaticCapture(1);
-  server = CameraServer.getServer();
-
-  camera1.setConnectionStrategy(ConnectionStrategy.kKeepOpen);
-  camera2.setConnectionStrategy(ConnectionStrategy.kKeepOpen);
-
-  
- leftMotor1 = new CANSparkMax(leftMotor1ID, MotorType.kBrushless);
- leftMotor0 = new CANSparkMax(leftMotor0ID, MotorType.kBrushless);
- rightMotor0 = new CANSparkMax(rightMotor0ID, MotorType.kBrushless);
- rightMotor1 = new CANSparkMax(rightMotor1ID, MotorType.kBrushless);
- armRetracter = new CANSparkMax(armRetracterID, MotorType.kBrushless);
- armRaiser = new CANSparkMax(armRaiserID, MotorType.kBrushless);
-
-
- MotorControllerGroup leftMotors = new MotorControllerGroup(leftMotor0, leftMotor1);
- MotorControllerGroup rightMotors = new MotorControllerGroup(rightMotor0, rightMotor1);
-
- leftMotors.setInverted(true);
- m_robotDrive = new DifferentialDrive(leftMotors, rightMotors);
-
- solenoid.set(DoubleSolenoid.Value.kForward);
-
- armRaiser.getEncoder().setPosition(0);
- armRetracter.getEncoder().setPosition(0);
- disableEncoder = false;
- 
-}
-
-
-
-
-@Override
-public void teleopPeriodic() {
-
-  if (m_stick.getRawButtonPressed(2)) {
-    if(cameraSwitch){
-      server.setSource(camera2);
-      cameraSwitch = false;
-    } else {
-      server.setSource(camera1);
-      cameraSwitch = true;
+  @Override
+  public void disabledPeriodic()
+  {
+    if (disabledTimer.hasElapsed(Constants.Drivebase.WHEEL_LOCK_TIME))
+    {
+      m_robotContainer.setMotorBrake(false);
+      disabledTimer.stop();
     }
   }
 
+  /**
+   * This autonomous runs the autonomous command selected by your {@link RobotContainer} class.
+   */
+  @Override
+  public void autonomousInit()
+  {
+    m_robotContainer.setMotorBrake(true);
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
- if(m_stick.getRawButton(8)){
-   percentPower = 1;
- } else if (m_stick.getRawButton(10)){
-   percentPower = 0.8;
- } else if(m_stick.getRawButton(12)){
-  percentPower += 0.1;
- } else if(m_stick.getRawButtonPressed(11)){
-   percentPower -= 0.1;
- }
-
-
-if (m_stick.getRawButton(4)){
-   armRetracter.set(0.8);
-} else if(m_stick.getRawButton(6) && inEncoderLimit){
-  armRetracter.set(-0.8);
-} else {
-   armRetracter.set(0);
-}
-
-if(m_stick.getRawButton(7)){
-  disableEncoder = true;
-  inEncoderLimit = true;
-}
-
-if(!disableEncoder){
-  if(armRetracter.getEncoder().getPosition() < 5){
-    inEncoderLimit = false;
-  } else {
-    inEncoderLimit = true;
-  }
-}
-
- if(m_stick.getRawButton(5)){
-   armRaiser.set(0.8);
- } else if (m_stick.getRawButton(3)){
-   armRaiser.set(-0.8);
- } else {
-   armRaiser.set(0);
- }
-
- if(m_stick.getRawButton(9)){
-  armRetracter.getEncoder().setPosition(0);
-  armRaiser.getEncoder().setPosition(0);
- }
-
-
- if(m_stick.getTriggerPressed()){
-   if(solenoid.get() == DoubleSolenoid.Value.kForward){
-     solenoid.set(DoubleSolenoid.Value.kReverse);
-   } else if (solenoid.get() == DoubleSolenoid.Value.kReverse){
-     solenoid.set(DoubleSolenoid.Value.kForward);
-   }
- }
-
- m_robotDrive.arcadeDrive( -m_stick.getY() * percentPower, m_stick.getTwist() * percentPower);
-
- SmartDashboard.putNumber("%Power", percentPower);
- SmartDashboard.putString("Value", solenoid.get().toString());
- SmartDashboard.putNumber("Raiser Rot", armRaiser.getEncoder().getPosition());
- SmartDashboard.putNumber("Puller Rot", armRetracter.getEncoder().getPosition());
- SmartDashboard.putBoolean("inEncoderLimit", inEncoderLimit);
- SmartDashboard.putBoolean("disableEncoder", disableEncoder);
-}
-
-
-public void autonomousInit(){
- elapsedTime.reset();
- elapsedTime.start();
-
- armRaiser.getEncoder().setPosition(0);
- armRetracter.getEncoder().setPosition(0);
-
- solenoid.set(DoubleSolenoid.Value.kForward);
-}
-
-
-
-
-public void autonomousPeriodic(){
-
-  if(armRaiser.getEncoder().getPosition() < 20 && elapsedTime.get() < 2 && elapsedTime.get() > 1){
-    armRaiser.set(0.8);
-    armRetracter.set(0);
-  } else if (armRaiser.getEncoder().getPosition() < 160 && elapsedTime.get() > 4 && elapsedTime.get() < 6){
-    armRaiser.set(0.8);
-  } else {
-    armRaiser.set(0);
-  }
-  
-  
-  if (armRetracter.getEncoder().getPosition() < 18 && elapsedTime.get() > 2 && elapsedTime.get() < 4){
-    armRetracter.set(0.8);
-    armRaiser.set(0);
-  } else if (armRetracter.getEncoder().getPosition() < 38 && elapsedTime.get() > 6 && elapsedTime.get() < 8){
-    armRetracter.set(0.8);
-  } else {
-    armRetracter.set(0);
-  }
-
-
-  if (elapsedTime.get() > 8 && elapsedTime.get() < 8.5){
-    solenoid.set(DoubleSolenoid.Value.kReverse);
-  }
-
-  if(elapsedTime.get() < 12.5 && elapsedTime.get() > 8.5){
-    solenoid.set(DoubleSolenoid.Value.kForward);
-    if(armRetracter.getEncoder().getPosition() > 10){
-      armRetracter.set(-0.5);
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null)
+    {
+      m_autonomousCommand.schedule();
     }
-    m_robotDrive.arcadeDrive(-0.5, 0);
+  }
+
+  /**
+   * This function is called periodically during autonomous.
+   */
+  @Override
+  public void autonomousPeriodic()
+  {
+  }
+
+  @Override
+  public void teleopInit()
+  {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null)
+    {
+      m_autonomousCommand.cancel();
+    }
+    m_robotContainer.setDriveMode();
+    m_robotContainer.setMotorBrake(true);
+  }
+
+  /**
+   * This function is called periodically during operator control.
+   */
+  @Override
+  public void teleopPeriodic()
+  {
+  }
+
+  @Override
+  public void testInit()
+  {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
+    try
+    {
+      new SwerveParser(new File(Filesystem.getDeployDirectory(), "swerve"));
+    } catch (IOException e)
+    {
+      throw new RuntimeException(e);
+    }
+  }
+
+  /**
+   * This function is called periodically during test mode.
+   */
+  @Override
+  public void testPeriodic()
+  {
+  }
+
+  /**
+   * This function is called once when the robot is first started up.
+   */
+  @Override
+  public void simulationInit()
+  {
+  }
+
+  /**
+   * This function is called periodically whilst in simulation.
+   */
+  @Override
+  public void simulationPeriodic()
+  {
   }
 }
- 
-}
-
